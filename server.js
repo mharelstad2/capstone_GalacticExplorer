cda9f881a80cf3b612dcee20fb14c89ffc513664
Marcus Harelstad <mharelstad2@student.cvtc.edu>
2023-11-29 21:19:55 -0600

    [Finishes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5163;
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
